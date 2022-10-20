e992032ac263663195c30c2285be334e1ea67a3a
sqd1998 <867998159@qq.com>
2022-10-20 11:10:48 +0800

    首页添加card

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,93 +1,108 @@
 <template>
-<div>
+  <div>
 
-  <el-row class="home"
-          :gutter="20">
-    <el-col :span="8">
-      <el-card class="box-card"
-               shadow="hover">
-        <div class="userInfo-top">
-          <img :src="userImg"
-               class="userImg">
-          <div class="userinfo">
-            <p class="name">Admin</p>
-            <p class="position">超级管理员</p>
-          </div>
-        </div>
-        <hr>
-        <div class="logininfo">
-          <p>上次登录时间：<span>2021-7-19</span></p>
-          <p>上次登录地点：<span>济南</span></p>
-        </div>
-      </el-card>
-      <el-card class="card2"
-               shadow="hover">
-        <el-table :data="shopDate">
-          <el-table-column prop="name"
-                           label="商品名称"></el-table-column>
-          <el-table-column prop="todayBuy"
-                           label="日销量"></el-table-column>
-          <el-table-column prop="monthBuy"
-                           label="月销量"></el-table-column>
-          <el-table-column prop="totalBuy"
-                           label="总销量"></el-table-column>
-
-        </el-table>
-      </el-card>
-    </el-col>
-    <el-col :span="16">
-      <div class="num">
-        <el-card v-for="item in countData"
-                 :key="item.index"
-                 :body-style="{display:'flex',padding: 0,width:'279px'}"
+    <el-row class="home"
+            :gutter="20">
+      <el-col :span="8">
+        <el-card class="box-card"
                  shadow="hover">
-          <div class="count-left">
-            <i class="icon"
-               :class="`el-icon-${item.icon}`"
-               :style="{background: item.color,width:'80px',height:'80px'}"></i>
+          <div class="userInfo-top">
+            <img :src="userImg"
+                 class="userImg">
+            <div class="userinfo">
+              <p class="name">Admin</p>
+              <p class="position">超级管理员</p>
+            </div>
           </div>
-          <div class="count-right">
-            <p class="name">{{item.name}}</p>
-            <p class="value">{{item.value}}</p>
+          <hr>
+          <div class="logininfo">
+            <p>上次登录时间：<span>2021-7-19</span></p>
+            <p>上次登录地点：<span>济南</span></p>
           </div>
         </el-card>
-      </div>
-      <!-- 折线图 -->
-      <el-card style="margin-top:20px;height:280px"
-               shadow="hover">
-        <echart :chartData="echartData.line"
-                style="height:280px;"></echart>
-      </el-card>
+        <el-card class="card2"
+                 shadow="hover">
+          <el-table :data="shopDate">
+            <el-table-column prop="name"
+                             label="商品名称"></el-table-column>
+            <el-table-column prop="todayBuy"
+                             label="日销量"></el-table-column>
+            <el-table-column prop="monthBuy"
+                             label="月销量"></el-table-column>
+            <el-table-column prop="totalBuy"
+                             label="总销量"></el-table-column>
 
-      <div class="graph">
-        <!-- 柱状图 -->
-        <el-card shadow="hover"
-                 class="graph-left">
-          <echart :chartData="echartData.bar"
-                  style="height:240px;"></echart>
+          </el-table>
         </el-card>
-        <!-- 饼状图 -->
-        <el-card shadow="hover"
-                 class="graph-right">
-          <echart :chartData="echartData.pie"
-                  style="height:240px;"></echart>
+      </el-col>
+      <el-col :span="16">
+        <div class="num">
+          <el-card v-for="item in countData"
+                   :key="item.index"
+                   :body-style="{display:'flex',padding: 0,width:'279px'}"
+                   shadow="hover">
+            <div class="count-left">
+              <i class="icon"
+                 :class="`el-icon-${item.icon}`"
+                 :style="{background: item.color,width:'80px',height:'80px'}"></i>
+            </div>
+            <div class="count-right">
+              <p class="name">{{item.name}}</p>
+              <p class="value">{{item.value}}</p>
+            </div>
+          </el-card>
+        </div>
+        <!-- 折线图 -->
+        <el-card style="margin-top:20px;height:280px"
+                 shadow="hover">
+          <echart :chartData="echartData.line"
+                  style="height:280px;"></echart>
         </el-card>
-        </el-card>
-      </div>
-      
-    </el-col>
-    
-  </el-row>
 
-  <el-row :gutter="20">
-  <el-col :span="8"><el-card></el-card></el-col>
-  <el-col :span="8"><el-card></el-card></el-col>
-  <el-col :span="8"><el-card></el-card></el-col>
-</el-row>
+        <div class="graph">
+          <!-- 柱状图 -->
+          <el-card shadow="hover"
+                   class="graph-left">
+            <echart :chartData="echartData.bar"
+                    style="height:240px;"></echart>
+          </el-card>
+          <!-- 饼状图 -->
+          <el-card shadow="hover"
+                   class="graph-right">
+            <echart :chartData="echartData.pie"
+                    style="height:240px;"></echart>
+          </el-card>
+          </el-card>
+        </div>
 
-</div>
-  
-  
+      </el-col>
+
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <el-card style="position:relative;" class="el-img-dev">
+          <a href="#">
+            <div class="img-desc">
+              <img src="../../assets/recruitment5.jpg"
+                   alt="">
+            </div>
+            <div class="img-text">
+              <h4 class="img-text-h4">招聘</h4>
+              <h5>诚邀东欧、中欧、亚太等全球的科学家、博士、竞赛人才、研发工程师、优秀大学生加盟</h5>
+            </div>
+          <div class="mask"></div>
+          </a>
+        </el-card>
+      </el-col>
+      <el-col :span="16">
+        <el-card></el-card>
+      </el-col>
+
+    </el-row>
+
+  </div>
+
 </template>
 
 <script>
@@ -129,28 +144,28 @@ export default {
           totalBuy: 10000
         },
         {
-          name: '小米',
+          name: '锤子',
+          todayBuy: 400,
+          monthBuy: 3200,
+          totalBuy: 400
+        },
+        {
+          name: '魅族',
+          todayBuy: 400,
+          monthBuy: 3200,
+          totalBuy: 4500
+        },
+        {
+          name: '红米',
           todayBuy: 400,
           monthBuy: 3200,
           totalBuy: 451000
         },
         {
-          name: '小米',
+          name: '荣耀',
           todayBuy: 400,
           monthBuy: 3200,
-          totalBuy: 451000
-        },
-        {
-          name: '小米',
-          todayBuy: 400,
-          monthBuy: 3200,
-          totalBuy: 451000
-        },
-        {
-          name: '小米',
-          todayBuy: 400,
-          monthBuy: 3200,
-          totalBuy: 451000
+          totalBuy: 751000
         }
       ],
       countData: [
@@ -207,8 +222,8 @@ export default {
     }
   },
   methods: {
-    async initEchart(){
-       const { data: res } = await getData()       
+    async initEchart() {
+      const { data: res } = await getData()
       // console.log(res)
       if (res.code === 200) {
         const order = res.data.orderData
@@ -251,10 +266,9 @@ export default {
           }
         ]
       }
-      
     }
   },
-  created(){
+  created() {
     this.initEchart()
   },
   mounted() {
@@ -265,13 +279,10 @@ export default {
     // .catch(err => {
     //   console.log(err);
     // })
-
-
-  //.then写法
+    //.then写法
     // getData().then(res => {
     //   const { code, data } = res.data
     //   console.log(data)
-
     //   if (code === 200) {
     //     const order = data.orderData
     //     // console.log(order)
@@ -318,7 +329,6 @@ export default {
     //   this.$http.get("http://www.liulongbin.top:3006/api/getbooks").then(res => {
     //       res = res.data;
     //       console.log(res);
-
     // })
   }
 }
@@ -398,6 +408,54 @@ export default {
     flex: 1;
     margin-left: 20px;
   }
+}
+
+.img-desc img {
+  display: inline-block;
+  width: 370px;
+  height: 270px;
+  transition: all 0.5s;
+}
+.img-text {
+  position: absolute;
+  bottom: 30px;
+  width: 335px;
+  height: auto;
+  padding: 20px 30px;
+  h5 {
+    font-size: 18px;
+    line-height: 1.5em;
+    color: #fff;
+    width: 300px;
+    font-weight: 900;
+
+
+  }
+   
+}
+.img-text-h4 {
+  font-size: 14px;
+  font-weight: 300;
+  color: #fff;
+}
+.el-img-dev:hover img {
+    transform: scale(1.1);
+
+}
+.el-img-dev:hover .img-text {
+    transform: translateY(-40px);
+    transition: all 0.5s;
+}
+.mask {
+    position: absolute;
+    left: 19px;
+    top: -3px;
+    width: 366px;
+    height: 276px;
+    background-image: linear-gradient( transparent, rgba(0, 0, 0, 0.6));
+    /* 渐变背景 */
+    opacity: 0;
+    transition: all 0.5s;
 }
 
 </style>
